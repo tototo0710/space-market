c46e88b3e438226a3517701b057e34066e68616e
tototo0710 <toto@katou-yuki-no-MacBook-Pro.local>
2017-10-10 19:12:21 +0900

    add

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/toto/projects/spacemarket/key/spacemarket02.pem']
+                  keys: ['/Users/toto/projects/spacemarket/key/.ssh/spacemarket02.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
